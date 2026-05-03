2c92f3c1d204ac0a36673d3879954b4d7e301908
José Rafael <jrmacedo70@gmail.com>
2026-05-03 11:41:09 -0300

    Novas questões resolvidas.

---
+++ exerciciosJavaDIO/maratona-exercicios/src/exercicios/poo/domain/Student.java
@@ -0,0 +1,49 @@
+package exercicios.poo.domain;
+
+/*Crie uma classe Aluno com os seguintes atributos:
+
+nome
+idade
+nota // três notas devem ser armazenadas
+
+Crie dois métodos:
+
+Para imprimir os dados
+Para calcular a média das notas e imprimir o resultado*/
+
+public class Student {
+    public String name;
+    public int age;
+    public double[] grade;
+
+    public void printStudent() {
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+
+        if(grade == null) {
+            return;
+        }
+
+        System.out.println("Grades: ");
+        for (int i = 0; i < 3; i++) {
+            System.out.println(grade[i]);
+        }
+
+        averageStudent();
+    }
+
+    public void averageStudent() {
+        if(grade == null) {
+            return;
+        }
+
+        double average = 0;
+
+        for (int i = 0; i < 3; i++) {
+            average += grade[i];
+        }
+        average /= grade.length;
+
+        System.out.printf("Student average: %.1f%n", average);
+    }
+}
---
+++ exerciciosJavaDIO/maratona-exercicios/src/exercicios/poo/main/StudentMain.java
@@ -0,0 +1,17 @@
+package exercicios.poo.main;
+
+import exercicios.poo.domain.Student;
+
+public class StudentMain {
+    public static void main(String[] args) {
+        Student student = new Student();
+        student.name = "Rafael";
+        student.age = 19;
+        student.grade = new double[3];
+        student.grade[0] = 10.0;
+        student.grade[1] = 5.5;
+        student.grade[2] = 7.3;
+
+        student.printStudent();
+    }
+}
